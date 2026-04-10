ceec402fafbe4e9601cb5420036d0bbad3695270
Léo FRANZ <leofranz2006@gmail.com>
2026-04-10 11:02:19 +0200

    Ajout de la prise en charge expérimentale du transport RTC via LiveKit dans la configuration, incluant l'activation de la fonctionnalité MSC4143.

--- setup.sh
+++ setup.sh
@@ -376,6 +376,10 @@ config['allow_public_rooms_without_auth']    = False
 # Element Call (VoIP/vidéo via MatrixRTC + LiveKit)
 livekit_url = os.environ.get('LIVEKIT_JWT_URL', '')
 if livekit_url:
+    # Flag expérimental : annonce org.matrix.msc4143 aux clients
+    config.setdefault('experimental_features', {})
+    config['experimental_features']['msc4143_rtc_transport'] = True
+    # Transport LiveKit effectif
     config['matrix_rtc'] = {
         'transports': [{
             'type': 'livekit',
